2e271a4e32c0f629a8007a44e55654a55f3a66f4
RavelMedeiros <ravel.medeiro@gmail.com>
2023-03-16 11:45:00 -0300

    nova commit

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 let tamanhoTela = window.innerWidth
 let nomePagina = window.location.pathname
 
-if (tamanhoTela <= 1200) {
+if (tamanhoTela <= 1200 && (nomePagina == '/Portifolio/' || nomePagina == '/Portifolio/index.html')) {
     let titulo = document.querySelector('.apresentacao__conteudo__titulo')
     let ImagemCelular = document.createElement('img')
-
-    if (nomePagina == '/Portifolio/' || nomePagina == '/Portifolio/index.html') {
-        ImagemCelular.src = "./assets/fotoPerfil.png"
-        ImagemCelular.alt = "Foto de perfil"
-        ImagemCelular.classList.add("apresentacao__imagem")
-        titulo.appendChild(ImagemCelular)
-    } else if (nomePagina == '/Portifolio/about.html') {
-        titulo = document.querySelector('.apresentacao')
-        let ImagemPc = document.createElement('img')
-        ImagemPc.src = "./assets/fotoPerfil.png"
-        ImagemPc.alt = "Foto de perfil"
-        ImagemPc.classList.add("apresentacao__imagem")
-        titulo.appendChild(ImagemPc)
-    }
+    ImagemCelular.src = "./assets/fotoPerfil.png"
+    ImagemCelular.alt = "Foto de perfil"
+    ImagemCelular.classList.add("apresentacao__imagem")
+    titulo.appendChild(ImagemCelular)
+} else if (tamanhoTela >= 1200) {
+    titulo = document.querySelector('.apresentacao')
+    let ImagemPc = document.createElement('img')
+    ImagemPc.src = "./assets/fotoPerfil.png"
+    ImagemPc.alt = "Foto de perfil"
+    ImagemPc.classList.add("apresentacao__imagem")
+    titulo.appendChild(ImagemPc)
 }
