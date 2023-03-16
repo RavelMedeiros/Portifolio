589251d62db6e5a5bf5460afcb9f3dc93cf264dd
RavelMedeiros <ravel.medeiro@gmail.com>
2023-03-16 09:43:56 -0300

    Alteração posição da imagem em mobile

---
+++ index.js
@@ -0,0 +1,22 @@
+let tamanhoTela = window.innerWidth
+let nomePagina = window.location.pathname
+
+if (tamanhoTela <= 1200) {
+    let titulo = document.querySelector('.apresentacao__conteudo__titulo')
+    let ImagemCelular = document.createElement('img')
+
+    if (nomePagina == "/index.html") {
+        ImagemCelular.src = "./assets/fotoPerfil.png"
+        ImagemCelular.alt = "Foto de perfil"
+        ImagemCelular.classList.add("apresentacao__imagem")
+        titulo.appendChild(ImagemCelular)
+    }
+} else {
+    let titulo = document.querySelector('.apresentacao')
+    let ImagemPc = document.createElement('img')
+
+    ImagemPc.src = "./assets/fotoPerfil.png"
+    ImagemPc.alt = "Foto de perfil"
+    ImagemPc.classList.add("apresentacao__imagem")
+    titulo.appendChild(ImagemPc)
+}
